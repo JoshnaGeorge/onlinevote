1619d1828a08f59ac07e69b3224c14ffa2d9be01
JoshnaGeorge <75978242+JoshnaGeorge@users.noreply.github.com>
2023-05-22 11:36:57 +0530

    onlinevote

---
+++ src/Helpers/partySymbols.js
@@ -0,0 +1,9 @@
+export const partySymbols = {
+  BJP: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6B92Ye3yT7qcspCQJfo7mQxTDr1vTD_alpQ&usqp=CAU",
+  SFI: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2w3sIIjgfrpU5tgyKxGVUpjlwpvMTRQTQxw&usqp=CAU",
+   KSU:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHjC5H5bHiVk4TBz5q_4ztf5UcXczHEt3tzg&usqp=CAU",
+  TRD: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHjC5H5bHiVk4TBz5q_4ztf5UcXczHEt3tzg&usqp=CAUhttps://www.cinejosh.com/newsimg/newsmainimg/1399829514_cycle.gif",
+  YRDC:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNF4SAEQJ4l2dcFDzZBIPAs9hn4v3IUU5o3w&usqp=CAU",
+};
